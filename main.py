d28d82b147d834ea8cf0b90c9d0bdff3613253c1
Tobias Llop <tobiasllop@gmail.com>
2023-08-31 15:57:47 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 14:29:42 2023
+
+@author: clinux01
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def proyectarPts(T, wz):
+    assert(T.shape == (2,2)) # chequeo de matriz 2x2
+    assert(T.shape[1] == wz.shape[0]) # multiplicacion matricial valida   
+    xy = wz.copy()
+    for j in range(len(wz[0])):
+            xy[0][j] = T[0][0] * wz[0][j] + T[0][1] * wz[1][j]
+            xy[1][j] = T[1][0] * wz[0][j] + T[1][1] * wz[1][j]
+    return xy
+
+def pointsGrid(corners):
+    # crear 10 lineas horizontales
+    [w1, z1] = np.meshgrid(np.linspace(corners[0,0], corners[1,0], 46),
+                        np.linspace(corners[0,1], corners[1,1], 10))
+
+    [w2, z2] = np.meshgrid(np.linspace(corners[0,0], corners[1,0], 10),
+                        np.linspace(corners[0,1], corners[1,1], 46))
+
+    w = np.concatenate((w1.reshape(1,-1),w2.reshape(1,-1)),1)
+    z = np.concatenate((z1.reshape(1,-1),z2.reshape(1,-1)),1)
+    wz = np.concatenate((w,z))
+                         
+    return wz
+          
+def vistform(T, wz, titulo=''):
+    # transformar los puntos de entrada usando T
+    xy = proyectarPts(T, wz)
+    if xy is None:
+        print('No fue implementada correctamente la proyeccion de coordenadas')
+        return
+    # calcular los limites para ambos plots
+    minlim = np.min(np.concatenate((wz, xy), 1), axis=1)
+    maxlim = np.max(np.concatenate((wz, xy), 1), axis=1)
+
+    bump = [np.max(((maxlim[0] - minlim[0]) * 0.05, 0.1)),
+            np.max(((maxlim[1] - minlim[1]) * 0.05, 0.1))]
+    limits = [[minlim[0]-bump[0], maxlim[0]+bump[0]],
+               [minlim[1]-bump[1], maxlim[1]+bump[1]]]             
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)         
+    fig.suptitle(titulo)
+    grid_plot(ax1, wz, limits, 'w', 'z')    
+    grid_plot(ax2, xy, limits, 'x', 'y')    
+    
+def grid_plot(ax, ab, limits, a_label, b_label):
+    ax.plot(ab[0,:], ab[1,:], '.')
+    ax.set(aspect='equal',
+           xlim=limits[0], ylim=limits[1],
+           xlabel=a_label, ylabel=b_label)
+
+
+def main():
+    print('Ejecutar el programa')
+    # generar el tipo de transformacion dando valores a la matriz T
+    #T = np.array([[2, 0.4],[0,1.]])
+    T = np.array([[(-1/4)*np.pi, 1],[1,(1/4)*np.pi]])
+    corners = np.array([[0,0],[100,100]])
+    wz = pointsGrid(corners)
+    vistform(T, wz, 'Deformar coordenadas')
+    
+    
+if __name__ == "__main__":
+    main()
